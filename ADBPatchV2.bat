bd79b4731df077c9f9d9b2e90f04aa4aaa3f7acc
MeOnGitHub123 <Scrat1986@gmx.de>
2015-08-26 18:28:57 +0200

    Update ADBPatchV2.bat
    fixed java errors.
    java-variable needs to be used correct:
    capital letters as set so in main script
    %-signs because of batch programming
    quotas because java-variable (which contains path) might contain empty chars

--- ADBPatchV2.bat
+++ ADBPatchV2.bat
@@ -6,11 +6,11 @@ echo Downloading latest WhatsApp
 bin\curl -o WhatsApp.apk http://www.whatsapp.com/android/current/WhatsApp.apk
 )
 echo I: Decompiling APK
-java -jar bin\apktool.jar d WhatsApp.apk
+"%JAVA%" -jar bin\apktool.jar d WhatsApp.apk
 echo I: Patching AndroidManifest.xml
 bin\fart.exe -q WhatsApp\AndroidManifest.xml allowBackup=\"false allowBackup=\"true
 echo I: Recompiling APK
-java -jar bin\apktool.jar b WhatsApp
+"%JAVA%" -jar bin\apktool.jar b WhatsApp
 echo I: Signing APK
-java -jar bin\signapk.jar crt\wa.pem crt\wa.pk8 WhatsApp\dist\WhatsApp.apk WhatsApp_Patched.apk
+"%JAVA%" -jar bin\signapk.jar crt\wa.pem crt\wa.pk8 WhatsApp\dist\WhatsApp.apk WhatsApp_Patched.apk
 echo I: Patching Done!
